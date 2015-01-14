26770039ab66197f43a53fba65515a2a2beb6f9a
universeview <universeview@pc>
2015-01-14 20:48:40 +0800

    fixed #1    order修改完成

--- src/servlet/demo/Usermanageservlet.java
+++ src/servlet/demo/Usermanageservlet.java
@@ -31,21 +31,7 @@ public class Usermanageservlet extends HttpServlet{
 		
 		 List usersList = null;
 		 
-		 try{
-			 
-			 GwapService  userservice = new UsersServiceImpl();			 
-		 	 usersList = userservice.getGwapList();
-		     toUserList(out, usersList);
-		     
-			} catch (Exception e){
-				toError(resp,e.getMessage());
-			}
-		
-	}
-
-
-	private void toUserList(PrintWriter out, List usersList) {
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		 out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
 		 out.println("<html>");
 		 out.println("	<head>");
 		 out.println("		<title>达内电子商务门户</title>");
@@ -178,63 +164,25 @@ public class Usermanageservlet extends HttpServlet{
 		 out.println("		  </tr>");
 		 out.println("		</table>");
 		 out.println("	</body>");
-		 out.println("</html>");
-		 
-		 
+		 out.println("</html>");		 
 		 out.close();
-	}
-	
-	
-	private void toError(HttpServletResponse resp, String message) throws IOException {
-		resp.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = resp.getWriter();
-		out.println("<html>    "); 
+		 
+//		 try{
+//			 
+//			 GwapService  userservice = new UsersServiceImpl();			 
+//		 	 usersList = userservice.getGwapList();
+//		     toUserList(out, usersList);
+//		     
+//			} catch (Exception e){
+//				toError(resp,e.getMessage());
+//			}
 		
-		 out.println("<head>    "); 
-		 out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />    "); 
-		 out.println("<title>网站正在建设中，请稍候再试</title>    "); 
-		 out.println("<style>    "); 
-		 out.println("body{font-family:Helvetica,Arial,sans-serif;margin:0;background-color:#CCC;background:-webkit-linear-gradient(#CCC,#AAA);background-attachment:fixed;}#cell{padding:40px;}#box{background-color:white;color:black;font-size:10pt;line-height:18px;margin:auto;max-width:800px;border-radius:5px;-webkit-box-shadow:2px 5px 12px#555;padding:20px;}    "); 
-		 out.println("ul{margin:0;padding-bottom:0;}    "); 
-		 out.println("li{padding-top:2px;}    "); 
-		 out.println("h1{font-size:18pt;line-height:30px;margin:0;}    "); 
-		 out.println(".right{color:#00FF00;text-align: right;}    "); 
-		 out.println("a{color:#B5B5B5;}    "); 
-		 out.println("</style>    "); 
-		 out.println("</head>    "); 
-		 out.println("<body id=\"t\">    "); 
-		 out.println("<div id=\"cell\">    "); 
-		 out.println("<div id=\"box\">    "); 
-		 out.println("<body bgcolor=#rrggbb>"); 
-		 out.println("<font bgcolor=#00FF00></font>");
-		 out.println("<font face=\"隶书\"></font>");
-		 out.println("<h1>有点小错误啊，请稍候再试好不好哇.....</h1>    "); 
-		 out.println("<p>    "); 
-		 out.println("<ul>    "); 
-		 out.println("<h3>");
-		 out.println("<li>粗枝大叶没有设置默认页</li>    "); 
-		 out.println("<br>");
-		 out.println("<li>贵人多忘事没有上传程序</li>    "); 
-		 out.println("<br>");
-		 out.println("<li>不干了网站荒废已经长草</li>    "); 
-		 out.println("<br>");
-		 out.println("<li>怒火攻心把网站咔嚓掉了</li>    "); 
-		 out.println("<br>");
-		 out.println("<li>有个二货把域名CNAME到这里</li>    "); 
-		 out.println("<br>");
-		 out.println("<br>");
-		 out.println("<li>要不去百度玩玩???</li>    "); 
-		 out.println("</h3>");
-		 out.println("</ul>    "); 
-		 out.println("</p>    "); 
-		 out.println("<body link=#rrggbb> ");
-		 out.println("<p class=\"right\">&copy; 2015 @<a href=\"http://www.baidu.com\">百度一下  你就知道:</a></p>    "); 
-		 out.println("</div>    "); 
-		 out.println("</div>    "); 
-		 out.println("</body>    "); 
-		 out.println("</html>    ");
-		out.close();
 	}
+
+
+
+	
+
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
